3d2066111e8962480fd325a73eae2ccfe3c9160b
Luu Hiep <luuhiep123mc@gmail.com>
2026-05-21 01:19:59 +0700

    feat: show user avatar when logged in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,12 @@ import "./Navbar.css"
 import { useContext } from "react"
 import { LanguageContext } from "../context/LanguageContext"
 import { logout } from "../utils/logout"
+import { useNavigate } from "react-router-dom"
 
 function Navbar() {
   const { lang, changeLanguage } = useContext(LanguageContext)
+  const navigate = useNavigate()
+  const isLoggedIn = !!localStorage.getItem("token")
 
   return (
     <nav className="navbar">
@@ -14,25 +17,11 @@ function Navbar() {
       </div>
 
       <ul className="menu">
-        <li>
-          <a href="#home">Home</a>
-        </li>
-
-        <li>
-          <a href="#timeline">Journey</a>
-        </li>
-
-        <li>
-          <a href="#skills">Skills</a>
-        </li>
-
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#timeline">Journey</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#contact">Contact</a></li>
 
         <li
           className="lang-btn"
@@ -41,9 +30,24 @@ function Navbar() {
           {lang.toUpperCase()}
         </li>
 
-        <button onClick={logout} className="logout-btn">
-          Logout
-        </button>
+        {isLoggedIn ? (
+          <>
+            <img
+              src="/user.png"
+              alt="User"
+              className="user-avatar"
+              onClick={logout}
+              title="Click to logout"
+            />
+          </>
+        ) : (
+          <button
+            className="logout-btn"
+            onClick={() => navigate("/login")}
+          >
+            Login
+          </button>
+        )}
       </ul>
     </nav>
   )
